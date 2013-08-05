b0777f3f5758c60f1c62725124f7223ae67c4f62
manmeetsingh984 <manmeet.singh984@gmail.com>
2013-08-06 00:14:01 +0530

    Update borderlayout.C

--- borderlayout.C
+++ borderlayout.C
@@ -9,19 +9,27 @@ class student : public WApplication
 {
  public:
 student(const WEnvironment & env):WApplication(env)
-{useStyleSheet("style.css");
+{
+ useStyleSheet("style.css");
+ //Creates a container on root.
  Wt::WContainerWidget *container = new Wt::WContainerWidget(root());
-container->setHeight(700);
-container->setStyleClass("yellow-box");
+ //height of container
+ container->setHeight(700);
+ //set style class to container
+ container->setStyleClass("yellow-box");
+
+//Makes a layout object of WBorderLayout. You used it to handle the layout of container
 
 Wt::WBorderLayout *layout = new Wt::WBorderLayout();
 container->setLayout(layout);
 
 const char *cell = "{1} ";
+//Added text at the top
 Wt::WText *item = new Wt::WText(Wt::WString(cell).arg("<h1> Layout Managers</h1>"));
 item->setStyleClass("black-box");
 layout->addWidget(item, Wt::WBorderLayout::North);
 
+//Added Image in the West
 
 Wt::WImage *image = new Wt::WImage("manmeet.png", root());
 item->setStyleClass("blue-box");
@@ -37,6 +45,8 @@ layout->addWidget(item, Wt::WBorderLayout::East);
 item = new Wt::WText(Wt::WString(cell).arg(""));
 item->setStyleClass("no-box");
 layout->addWidget(item, Wt::WBorderLayout::South);
+
+//Added text in center
 
 item = new Wt::WText(Wt::WString(cell).arg(" Usually, CSS is a sufficient and convenient way to indicate how to layout widgets within a container widget, and we recommend to get accustomed with CSS for basic layout and styling.There are however several situations when CSS is not effective to create a layout, especially for complex web applications. In particular, when placing contents within a dialog it is often necessary to adjust its contents to available vertical space, and it is often desirable to distribute excess space to a particular widget while keeping other widgets to their preferred size. It is for these reasons that Wt has a comprehensive set of layout managers that use JavaScript to compute and apply layout managent to children of a WContainerWidget.Wt provides the typical box, grid or border layout management options and layout managers and CSS can safely be intermixed. "));
 item->setStyleClass("green-box");
